fb5ff1ca504ddf2fa1c3b0c6fe7104da93e8d0df
Thalles Henrique <thalleshenrique.na@gmail.com>
2020-02-09 15:28:18 -0300

    versao 01

--- main.js
+++ main.js
@@ -8,5 +8,34 @@ function activateLink(element){
     console.log(element)
 
     element.classList.add('active')
+}
 
+function openProjects(element){
+
+    var embedded = document.getElementById("embedded")
+
+    if (embedded.classList.contains("container-opened")){
+        embedded.classList.remove("container-opened")
+        embedded.classList.add("container-closed");
+        setTimeout(function(){
+            embedded.style.display = "none"
+        }, 500)
+        element.innerHTML = "Ver outros projetos"
+    }else{
+        fetch("/projects.html" /*, options */)
+        .then((response) => response.text())
+        .then((html) => {
+            embedded.style.display = "block"
+            embedded.innerHTML = html
+            embedded.classList.remove("container-closed")
+            embedded.classList.add("container-opened");
+            element.innerHTML = "Ver menos projetos"
+        })
+        .catch((error) => {
+            console.warn(error);
+        });
+    }
+
+
+    
 }
